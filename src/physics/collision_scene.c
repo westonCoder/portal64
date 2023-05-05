5335ede765df443c3be75a470978443a4f54cd52
Weston Salinas <weston.l.salinas@gmail.com>
2023-05-05 13:20:23 -0500

    Player Contact Freeze Fix
    - player producing contact on grabable objects disabled
    - grabbable objects producing contact on player has been preserved
    - player no longer gets pushed into ground by objects
    - video attached
    
    Fixes #136

--- src/physics/collision_scene.c
+++ src/physics/collision_scene.c
@@ -498,6 +498,10 @@ void dynamicBroadphaseSort(union DynamicBroadphaseEdge* edges, union DynamicBroa
 }
 
 void collisionObjectCollidePairMixed(struct CollisionObject* a, struct Vector3* aPrevPos, struct Box3D* sweptA, struct CollisionObject* b, struct Vector3* bPrevPos, struct Box3D* sweptB, struct ContactSolver* contactSolver) {
+    if (a->body->flags & RigidBodyIsPlayer && b->body->flags & RigidBodyFlagsGrabbable){
+        return;
+    }
+
     if (a->manifoldIds & b->manifoldIds) {
         collisionObjectCollideTwoObjects(a, b, contactSolver);
     } else {
